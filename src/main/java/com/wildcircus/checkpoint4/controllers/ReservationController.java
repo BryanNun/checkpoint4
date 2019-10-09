d48e99072c8cf6a7a7d8c50343bfaf0d1e783f0a
BryanNun <nuninger.bryan@gmail.com>
2019-10-09 15:23:05 +0200

    Added ReservationController

--- src/main/java/com/wildcircus/checkpoint4/controllers/ReservationController.java
+++ src/main/java/com/wildcircus/checkpoint4/controllers/ReservationController.java
@@ -2,7 +2,7 @@ package com.wildcircus.checkpoint4.controllers;
 
 import javax.validation.Valid;
 
-import com.wildcircus.checkpoint4.repositories.ResRepository;
+import com.wildcircus.checkpoint4.repositories.ReservationRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class ResController {
+public class ReservationController {
 
     @Autowired
-    private ResRepository resRepository;
+    private ReservationRepository reservationRepository;
 
     @GetMapping("/reservation")
     public String browse(Model model){
@@ -34,7 +34,7 @@ public class ResController {
             return "redirect:/reservations/create";
         }
          else {
-            resRepository.save(reservation);
+            reservationRepository.save(reservation);
             return "redirect:/reservations";
         }
     }
