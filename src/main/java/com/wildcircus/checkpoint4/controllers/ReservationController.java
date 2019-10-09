9fcac543d72319dcfbcc7bdae14b1fa9771a1a09
BryanNun <nuninger.bryan@gmail.com>
2019-10-09 16:21:15 +0200

    Added PostMapping

--- src/main/java/com/wildcircus/checkpoint4/controllers/ReservationController.java
+++ src/main/java/com/wildcircus/checkpoint4/controllers/ReservationController.java
@@ -2,16 +2,14 @@ package com.wildcircus.checkpoint4.controllers;
 
 import javax.validation.Valid;
 
+import com.wildcircus.checkpoint4.entities.Reservation;
 import com.wildcircus.checkpoint4.repositories.ReservationRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ReservationController {
@@ -23,4 +21,10 @@ public class ReservationController {
     public String browse(Model model){
         return "reservation";
     }
+
+    @PostMapping("/reservation/create")
+    public String store(@Valid Reservation reservation) {
+       reservation = reservationRepository.save(reservation);
+       return "redirect:/users/" + reservation.getId();
+   }
 }
