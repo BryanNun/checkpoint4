8512fc83447beff8c8cf18bede324f208fbe1efd
BryanNun <nuninger.bryan@gmail.com>
2019-10-10 15:50:48 +0200

    Added scroll effect

---
+++ src/main/resources/static/scrollreveal.js
@@ -0,0 +1,27 @@
+const scroll = ScrollReveal({
+    duration:1500,
+    reset:true
+    });
+    
+  scroll.reveal ('header'); 
+
+  scroll.reveal ('.Titretarifs'); 
+
+  scroll.reveal ('table'); 
+
+  scroll.reveal ('h2');
+
+  scroll.reveal ('form'); 
+
+  scroll.reveal ('label');
+
+  scroll.reveal ('select'); 
+
+  scroll.reveal ('.my-3 d-flex justify-content-center'); 
+  
+  scroll.reveal ('.custom-select');
+  
+  scroll.reveal ('button');
+
+  scroll.reveal ('footer');
+  
